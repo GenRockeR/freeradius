5506b9728f36e9489d51a0e96420386b411c42f6
Sean Enck <enckse@gmail.com>
2017-01-24 17:15:41 -0500

    support packet reporting (#6)

--- mods-config/python/utils/report.py
+++ mods-config/python/utils/report.py
@@ -2,11 +2,33 @@
 """reports for processing."""
 
 import argparse
+import sqlite3
+
+OUT_PACKETS = 'Acct-Output-Packets'
+IN_PACKETS = 'Acct-Input-Packets'
 
 
-def _packets(database):
+def _packets(cursor):
     """print information about packet thru-put."""
-    print database
+    cursor.execute("select line, key, val from data where key = '{0}' or key = '{1}'".format(OUT_PACKETS, IN_PACKETS))
+    queries = {}
+    for row in cursor.fetchall():
+        num = row[0]
+        key = row[1]
+        val = row[2]
+        query = "select substr(date, 0, 11) as date, val, {0} as packets from data where line = {1} and key = 'User-Name'".format(val, num)
+        if key not in queries:
+            queries[key] = []
+        queries[key].append(query)
+    template = "select val, date, sum(packets) from ({0}) as X group by val, date order by val, date"
+    for q in queries:
+        query = template.format(" UNION ".join(queries[q]))
+        cursor.execute(query)
+        print q
+        print "==="
+        for row in cursor.fetchall():
+            print "{:>15}{:>15}{:>15}".format(row[0], row[1], row[2])
+        print
 
 
 # available reports
@@ -24,12 +46,14 @@ def main():
         print "please give some reports to run..."
         exit(1)
     execute = args.reports
+    conn = sqlite3.connect(args.database)
+    curs = conn.cursor()
     for item in execute:
         if item in available:
-            available[item](args.database)
+            available[item](curs)
         else:
             print "unknown report..." + item
-            exit(1)
+    conn.close()
 
 if __name__ == "__main__":
     main()
