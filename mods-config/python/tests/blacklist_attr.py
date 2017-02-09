629ceed112d49667b3254c5e1efcda4dc0ea320f
Sean Enck <enckse@gmail.com>
2017-02-09 08:36:28 -0500

    expected output

---
+++ mods-config/python/tests/blacklist_attr.py
@@ -0,0 +1,2 @@
+import __config__
+obj = __config__.Blacklist("blah")
---
+++ mods-config/python/tests/user_attr.py
@@ -0,0 +1,6 @@
+import __config__
+normal = __config__.Assignment()
+normal.macs = ["001122334455"]
+normal.password = "12345678910aaaaaaaaaaaaaaa111213141516etcabc"
+normal.vlan = "dev"
+normal.attrs = ["blah"]
