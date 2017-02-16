6a754736297c414778427ca0303e39882d57f908
Sean Enck <enckse@gmail.com>
2017-02-16 18:06:37 -0500

    tests for expired and disabled user/macs

---
+++ mods-config/python/tests/user_disable.py
@@ -0,0 +1,9 @@
+import __config__
+import common
+disabled = "11ff33445566"
+normal = __config__.Assignment()
+normal.macs = [common.VALID_MAC]
+normal.password = "12345d678910aaaaaaaaaaaaaaa111213141516etc"
+normal.bypass = [disabled]
+normal.disable = { disabled: "2017-01-01" }
+normal.vlan = "dev"
---
+++ mods-config/python/tests/user_expire.py
@@ -0,0 +1,7 @@
+import __config__
+import common
+normal = __config__.Assignment()
+normal.macs = [common.VALID_MAC]
+normal.password = "12345aaa678910aaaaaaaaaaaaaaa111213141516etc"
+normal.vlan = "dev"
+normal.expired = "2017-01-01"
