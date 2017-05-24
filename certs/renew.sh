b58bf6ba4be788023e4fd3e7ea96791b8f518012
Sean Enck <enckse@gmail.com>
2017-05-24 16:32:15 -0400

    resolves #12, need ocsp cnf file AND put passwords into CNF and generate passwords.mk

--- certs/renew.sh
+++ certs/renew.sh
@@ -4,17 +4,23 @@ function print-bar()
     echo "
 ==================
 $@
-=================="
+==================
+Press <Enter> to continue"
     read
 }
 cd /etc/raddb/certs
 print-bar "this script assumes all passwords are the same, this is viable for a restricted area LAN at best. you will be prompted for each password (use the same one), make sure passwords.mk has already been updated."
-if [ ! -f passwords.mk ]; then
-    echo "no passwords file..."
-    exit -1
-fi
 echo "removing previous certs"
 rm -f *.pem *.der *.csr *.crt *.key *.p12 serial* index.txt*
+echo -n Password:
+read -s password
+for f in $(ls *.cnf); do
+    sed -i "s/input_password =/input_password = $password/g" $f
+    sed -i "s/output_password =/output_password = $password/g" $f
+done
 echo "rebuilding certs"
 ./bootstrap
+for f in $(ls *.cnf); do
+    git checkout -- $f
+done
 print-bar "before start/restarting radius, update the clients.conf and add [certprivkey = <password>]"
