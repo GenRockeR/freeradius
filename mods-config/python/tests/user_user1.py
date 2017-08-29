59ffc7730e1831e6975b249aaef3818759cb26af
Sean Enck <enckse@gmail.com>
2017-08-29 12:32:20 -0400

    reworking tests

--- mods-config/python/tests/user_user1.py
+++ mods-config/python/tests/user_user1.py
@@ -3,11 +3,11 @@ import __config__
 import common
 normal = __config__.Assignment()
 normal.macs = [common.VALID_MAC]
-normal.password = "12345678910aaaaaaaaaaaaaaa111213141516etc"
+normal.password = "1057687797.863093848|4050535148.2760919111|2943557348.3572403425"
 normal.bypass = ["112233445566"]
 normal.vlan = "dev"
 
 admin = __config__.Assignment()
 admin.macs = normal.macs
-admin.password = normal.password + "admin"
+admin.password = 1057687797.863093848|4050535148.2760919111|2943557348.3572403425|1046040092.2821704132|3813347290.3145056057
 admin.vlan = "prod"
--- mods-config/python/tests/user_user2.py
+++ mods-config/python/tests/user_user2.py
@@ -2,10 +2,11 @@
 import __config__
 normal = __config__.Assignment()
 normal.macs = ["001122334455"]
-normal.password = "12345678910aaaaaaaaaaaaaaa111213141516user2"
+normal.password = "1057687797.863093848|4050535148.2760919111|64205524.3092519096"
 normal.vlan = "dev"
 
 admin = __config__.Assignment()
 admin.macs = normal.macs
 admin.password = normal.password + "user2"
 admin.vlan = "prod"
+admin.password = "1057687797.863093848|4050535148.2760919111|64205524.3092519096|1046040092.2821704132|3813347290.3145056057"
