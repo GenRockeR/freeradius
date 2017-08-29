c6f33ffff0a9b12fb6f6d92edc4f7c93a64b845f
Sean Enck <enckse@gmail.com>
2017-08-29 13:46:40 -0400

    reworking tests

--- mods-config/python/tests/common.py
+++ mods-config/python/tests/common.py
@@ -1,12 +1,3 @@
 """Common testing definitions."""
 import keying
 VALID_MAC = "001122334455"
-
-
-def ready(obj):
-    """Called on object setup/ready."""
-    if obj.password.endswith('admin'):
-        obj.password = obj.password[0:-1]
-    if obj.password:
-        obj.password = key.change_password(None, "test", obj.password))
-    return obj
