386e5bdfe526cab239690c064f06d167f05ac57e
Sean Enck <enckse@gmail.com>
2017-10-05 11:24:21 -0400

    include ability to provide config to baseline from

--- mods-config/python/utils/report_auths.py
+++ mods-config/python/utils/report_auths.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python
 """Report authorization information."""
 import datetime as dt
+import argparse
+import json
+
+_KEY = "->"
+
+def _new_key(user, mac):
+    return "{}{}{}".format(user, _KEY, mac)
 
 
 def _file(day_offset, auth_info):
@@ -26,24 +33,40 @@ def _file(day_offset, auth_info):
                         calling_station = data.index("Calling-Station-Id") + 22
                         calling = data[calling_station:]
                         calling = calling[:calling.index(")") - 1]
-                        calling = replace(":", "").replace("-", "").lower()
-                        key = "{}->{}".format(user_start, calling)
+                        calling = calling.replace(":",
+                                                  "").replace("-",
+                                                              "").lower()
+                        key = _new_key(user_start, calling)
                         uuid_log[uuid] = key
                         if key not in auth_info:
-                            auth_info[key] = None
+                            auth_info[key] = "denied"
 
 
 def main():
     """Accept/reject reporting."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--days", type=int, default=10)
+    parser.add_argument("--config",
+                        type=str,
+                        default="/etc/raddb/mods-config/python/network.json")
+    args = parser.parse_args()
+    config = None
     authd = {}
+    with open(args.config) as f:
+        j = json.loads(f.read())
+        users = j["users"]
+        for u in users:
+            for m in users[u]["macs"]:
+                k = _new_key(u, m)
+                authd[k] = "n/a"
     today = dt.date.today()
-    for x in reversed(range(1, 11)):
+    for x in reversed(range(1, args.days + 1)):
         _file("{}".format(today - dt.timedelta(days=x)), authd)
-    print("| user | mac | date |")
+    print("| user | mac | last |")
     print("| ---  | --- | ---  |")
     for item in sorted(authd.keys()):
         on = authd[item]
-        parts = item.split("->")
+        parts = item.split(_KEY)
         if on is None:
             on = ""
         print("| {} | {} | {} |".format(parts[0], parts[1], on))
