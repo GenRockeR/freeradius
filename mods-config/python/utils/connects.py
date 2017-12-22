da42dbea2bd038cb2e2b104658fc8de9c1a7202e
Sean Enck <enckse@gmail.com>
2017-12-22 11:50:40 -0500

    more coverage

--- mods-config/python/utils/connects.py
+++ mods-config/python/utils/connects.py
@@ -4,17 +4,37 @@ import os
 import sqlite3 as sl
 import sys
 import datetime
+from ast import literal_eval as make_tuple
+import wrapper
 
 
-def _extract(line, attr):
-    key = "'{}'".format(attr)
-    if key in line:
-        idx = line.index(key)
-        sub = line[idx + len(key) + 2:len(line)]
-        val = sub.strip().split(" ")[0].replace("'", "").replace(")", "").replace(",", "")
-        return val
+def _object(user, port, nas_ip, mac):
+    return (user, port, nas_ip, wrapper.convert_mac(mac))
 
+def _get(key, existing, tple):
+    if existing:
+        return existing
+    if tple[0] == key:
+        return tple[1]
 
+def _report(conn, tracked):
+    """Report on new entries as found."""
+    curs = conn.cursor()
+    curs.execute("""
+CREATE TABLE IF NOT EXISTS tracked
+(
+    date text,
+    user text,
+    nasp text,
+    nasi text
+)
+""")
+    date = datetime.datetime.now().strftime("%Y-%m-%d")
+    for row in curs.execute("select user, nasp, nasi from tracked"):
+        t = _object(u, p, n)
+        if t in tracked:
+            tracked.remove(t)
+    print(tracked) 
 
 def main():
     """Main entry."""
@@ -22,17 +42,26 @@ def main():
     parser.add_argument("--db", default=".")
     args = parser.parse_args()
     lines = sys.stdin.readlines()
-    tracked = []
     with sl.connect(os.path.join("test.db")) as c:
+        tracked = []
         for l in lines:
-            u = _extract(l, "User-Name")
-            if u:
-                p = _extract(l, "NAS-Port")
-                if p:
-                    n = _extract(l, "NAS-IP-Address")
-                    if n:
-                        tracked.append((u, p, n))
-    print(set(tracked))
+            if "->" in l:
+                t = make_tuple(l.split("->")[1].strip())
+                user = None
+                nasp = None
+                nasi = None
+                macs = []
+                for k in t:
+                    user = _get("User-Name", user, k)
+                    nasp = _get("NAS-Port", nasp, k)
+                    nasi = _get("NAS-IP-Address", nasi, k)
+                    mac = _get("Calling-Station-Id", None, k)
+                    if mac:
+                        macs.append(mac)
+                macs = list(set(macs))
+                for mac in macs:
+                    tracked.append(_object(user, nasp, nasi, mac))
+        _report(c, list(set(tracked)))
 
 
 
