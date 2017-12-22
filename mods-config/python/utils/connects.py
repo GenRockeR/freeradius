676c3f6ba2adc85ddb80a9393a5f7208b4c31f35
Sean Enck <enckse@gmail.com>
2017-12-22 11:18:01 -0500

    working forward

--- mods-config/python/utils/connects.py
+++ mods-config/python/utils/connects.py
@@ -5,18 +5,33 @@ import sqlite3 as sl
 import sys
 
 
+def _extract(line, attr):
+    key = "'{}'".format(attr)
+    if key in line:
+        idx = line.index(key)
+        sub = line[idx + len(key) + 2:len(line)]
+        val = sub.strip().split(" ")[0].replace("'", "").replace(")", "").replace(",", "")
+        return val
+
+
+
 def main():
     """Main entry."""
     parser = argparse.ArgumentParser()
     parser.add_argument("--db", default=".")
     args = parser.parse_args()
     lines = sys.stdin.readlines()
-    report = []
+    tracked = []
     with sl.connect(os.path.join("test.db")) as c:
         for l in lines:
-            if "User-Name" in l and "AUTHORIZE" in l:
-                print(l)
-
+            u = _extract(l, "User-Name")
+            if u:
+                p = _extract(l, "NAS-Port")
+                if p:
+                    n = _extract(l, "NAS-IP-Address")
+                    if n:
+                        tracked.append((u, p, n))
+    print(set(tracked))
 
 
 
